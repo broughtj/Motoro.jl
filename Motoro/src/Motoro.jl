b120d1c7a81b7e0a263941f25420763f71370669
Tyler Brough <broughtj@gmail.com>
2026-01-22 21:12:16 -0700

    Motoro plays games

--- Motoro/src/Motoro.jl
+++ Motoro/src/Motoro.jl
@@ -1,5 +1,26 @@
 module Motoro
 
-greet() = print("Hello DATA 5690/6690!")
+function game() 
+    secret_number = rand(1:100)
+    tries = 0
+    guess = 0
+
+    println("I'm thinking of a number between 1 and 100.")
+    println("Can you guess it?")
+
+    while guess != secret_number
+        print("Your guess: ")
+        guess = parse(Int64, readline())
+        tries += 1
+
+        if guess < secret_number
+            println("Too low. Try again.")
+        elseif guess > secret_number
+            println("Too high. Try again.")
+        else 
+            println("Congratulations. You got it!")
+        end 
+    end
+end 
 
 end # module Motoro
